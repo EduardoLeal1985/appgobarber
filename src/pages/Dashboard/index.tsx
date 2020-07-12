fcd94a461f7ad797f3a356854cc4a97726914d5b
EduardoLeal1985 <eduleal2005@gmail.com>
2020-07-12 08:09:55 -0300

    Aula 135 - Conectando com a API

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { useAuth } from '../../hooks/auth';
+
+const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
+  return (
+    <View>
+      <Text>Dashboard</Text>
+      <Button title="Sair" onPress={signOut} />
+    </View>
+  );
+};
+
+export default Dashboard;
